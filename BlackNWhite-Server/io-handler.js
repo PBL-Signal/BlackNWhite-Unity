25a0976d11fd80111c100abecd86f23bdef05dc7
woogy4231 <woogy4231@naver.com>
2022-07-18 14:08:08 +0900

    add white team vuln

--- BlackNWhite-Server/io-handler.js
+++ BlackNWhite-Server/io-handler.js
@@ -1064,7 +1064,7 @@ module.exports = (io) => {
 
             // 게임 시간 타이머 
             io.sockets.in(socket.room).emit('Timer START');
-            timerId = setInterval(function(){
+            timerId = setInterval(async function(){
                 min = parseInt(time/60);
                 sec = time%60;
                 // console.log("TIME : " + min + "분 " + sec + "초");
@@ -1076,8 +1076,9 @@ module.exports = (io) => {
                     clearInterval(pitaTimerId);
 
                     // 게임종료 -> 점수 계산 함수 호출
+                    let roomTotalJsonFinal = JSON.parse(await jsonStore.getjson(socket.room));
                     io.sockets.in(socket.room).emit('Load_ResultPage');
-                    socket.on('Finish_Load_ResultPage', ()=> { TimeOverGameOver(socket, roomTotalJson); });               
+                    socket.on('Finish_Load_ResultPage', ()=> { TimeOverGameOver(socket, roomTotalJsonFinal); });               
                     
                 }
             }, 1000);
@@ -3310,7 +3311,7 @@ module.exports = (io) => {
             let seconds = today.getSeconds();  // 초
             let now = hours+":"+minutes+":"+seconds;
             var companyIdx =  attackJson.companyName.charCodeAt(7) - 65;
-            var monitoringLog = {time: now, nickname: "", targetCompany: attackJson.companyName, targetSection: sectionNames[companyIdx][attackJson.sectionIndex], actionType: "Detected", detail: attack_name_list[delIndex]+"공격이 탐지 되었습니다."};
+            var monitoringLog = {time: now, nickname: "", targetCompany: attackJson.companyName, targetSection: sectionNames[companyIdx][attackJson.sectionIndex], actionType: "Detected", detail: attack_name_list[attackJson.attackIndex]+"공격이 탐지 되었습니다."};
 
             whiteLogJson[0].push(monitoringLog);
             await jsonStore.updatejson(whiteLogJson[0], socket.room+":whiteLog");
@@ -3674,6 +3675,7 @@ module.exports = (io) => {
         // 남은 피타
         var blackPitaNum = roomTotalJson[0]["blackTeam"]["total_pita"];
         var whitePitaNum = roomTotalJson[0]["whiteTeam"]["total_pita"];
+
 
         // 화이트팀 : (남은 회사 * 1000) + 남은 피타    // 블랙팀 : (파괴한 회사 * 1000) + 남은 피타
         var whiteScore = (aliveCnt * 1000) + whitePitaNum;

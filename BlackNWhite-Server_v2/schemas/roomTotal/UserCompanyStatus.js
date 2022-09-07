e3d63e64fa2ce3d5fb3ba84b9d16469cba7ee727
HyeM207 <63229014+HyeM207@users.noreply.github.com>
2022-09-08 01:10:37 +0900

    Revert "Copy Project"
    This reverts commit 4f3f0ba8c58112e329d559c7d4e887095df5c8be.

--- BlackNWhite-Server_v2/schemas/roomTotal/UserCompanyStatus.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const UserCompanyStatus = new Schema({
-    warnCnt    : { type : Number, required : true },
-    detectCnt    : { type : Array, required : true },
-})
-
-module.exports = mongoose.model('UserCompanyStatus', UserCompanyStatus);
